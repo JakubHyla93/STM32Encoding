e70b6bb304b390e90a2eab9d0ea6c68949512c47
Jakub Hyla <jakubhyla93@gmail.com>
2020-04-04 23:29:32 +0200

    inital

---
+++ PWM/definesPWM.h
@@ -0,0 +1,29 @@
+/*
+ * definesPWM.h
+ *
+ *  Created on: Apr 4, 2020
+ *      Author: jakub
+ */
+
+#ifndef DEFINESPWM_H_
+#define DEFINESPWM_H_
+
+#define PWM_val_1 1
+#define PWM_val_2 2
+#define PWM_val_3 3
+#define PWM_val_4 4
+#define PWM_val_5 5
+#define PWM_val_6 6
+#define PWM_val_7 7
+#define PWM_val_8 8
+#define PWM_val_9 9
+#define PWM_val_10 10
+#define PWM_val_11 11
+#define PWM_val_12 12
+#define PWM_val_13 13
+#define PWM_val_14 14
+#define PWM_val_15 15
+#define PWM_val_16 16
+
+
+#endif /* DEFINESPWM_H_ */
